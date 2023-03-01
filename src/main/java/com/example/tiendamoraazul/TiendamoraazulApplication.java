0dc257fcb6b1c9f41d17f9808a7ef52520513201
OrionAlzate <orion_alzate@hotmail.com>
2023-02-28 20:36:11 -0500

    proyecto iniciado, clase Producto

---
+++ src/main/java/com/example/tiendamoraazul/TiendamoraazulApplication.java
@@ -0,0 +1,13 @@
+package com.example.tiendamoraazul;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TiendamoraazulApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TiendamoraazulApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/tiendamoraazul/TiendamoraazulApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.tiendamoraazul;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TiendamoraazulApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
